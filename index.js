c1b0bd2c70e99878bb1395bb3124285f6216fcf2
Luis <luisopentec@gmail.com>
2021-10-19 11:32:00 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = 3000;
+const host = '0.0.0.0';
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+
+app.listen(port, host)
